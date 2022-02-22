8c5a2244a0fffca9538c70939a323f107aab5b05
SatouKz <alexander.ajalla404@gmail.com>
2022-02-22 19:46:52 -0300

    advance

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
-import React from 'react'
-import {Container , Navbar, Button , Form} from 'react-bootstrap'
+import "./App.css";
+import Cards from "./components/Cards";
 
-const App = () => {
+function App() {
   return (
-    <>
-    <Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src="./images/hf.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-      React Bootstrap
-      </Navbar.Brand>
-    </Container>
-  </Navbar>
-
-  <Form>
-    <Container>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-    </Container>
-
-</Form>
-  </>
-    )
+    <div className="App">
+      <Cards />
+    </div>
+  );
 }
 
-export default App
+export default App;
